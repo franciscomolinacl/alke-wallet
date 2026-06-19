7a7b9cddceaaf195ccee921256455cf5067f5241
Francisco <0franciscomolina0@gmail.com>
2026-06-19 01:41:53 -0400

    feat: creacion de pagina transacciones/historial y mejoras en CSS

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -606,6 +606,81 @@ function mostrarContactosEnCheck(textoBuscar = "") {
     });
 }
 
+export function historialPag() {
+  leerTransacciones(); 
+  $(document).on("change", "#filtro-movimientos", function () {
+    const opcionSeleccionada = $(this).val(); 
+
+    if (opcionSeleccionada === "todos") {
+      $(".tarjeta-movimiento").fadeIn(300);
+    } else {
+      $(".tarjeta-movimiento").hide();
+      $(`.tarjeta-movimiento.${opcionSeleccionada}`).fadeIn(300); 
+    }
+  });
+}
+
+function leerTransacciones(){
+    const contenedor = $("#contenedor-transacciones");
+  if (!contenedor.length) return;
+
+  const datosStorage = localStorage.getItem("transacciones");
+
+  if (!datosStorage) {
+    contenedor.html(`
+      <div class="card mb-2 shadow-sm animate-vacio">
+        <div class="card-body d-flex justify-content-between align-items-center text-muted">
+          Aún no registras movimientos.
+        </div>
+      </div>
+    `);
+    return;
+  }
+
+  const lista = JSON.parse(datosStorage);
+  lista.sort((a, b) => {
+    const arreglarFecha = (fechaStr) => {
+      const [fechaPart, horaPart] = fechaStr.trim().split(" ");
+      const [dia, mes, anio] = fechaPart.includes("-")
+        ? fechaPart.split("-")
+        : fechaPart.split("/");
+      return `${anio}-${mes}-${dia} ${horaPart || ""}`.trim();
+    };
+    return (
+      new Date(arreglarFecha(b.fechayhora)) -
+      new Date(arreglarFecha(a.fechayhora))
+    );
+  });
+  contenedor.empty(); 
+
+  $.each(lista, function (index, tx) {
+    const tipoNormalizado = tx.tipo.toLowerCase();
+    const colorMonto =
+      tipoNormalizado === "deposito" ? "text-success" : "text-danger";
+    const signo = tipoNormalizado === "deposito" ? "+" : "-";
+    const montoFormateado = Number(tx.monto).toLocaleString("es-CL"); // Formateo a moneda
+
+    const estructuraTarjeta = `
+      <div class="card mb-2 shadow-sm tarjeta-movimiento ${tx.tipo.toLowerCase()}">
+          <div class="card-body d-flex justify-content-between align-items-center">
+              <div>
+                  <h6 class="mb-0 fw-bold">${tx.contacto}</h6>
+                  <small class="text-muted">${tx.fechayhora}</small>
+              </div>
+              <div class="text-end">
+                  <span class="fw-bold ${colorMonto}">${signo}$${montoFormateado}</span>
+                  <br>
+                  <small class="badge bg-light text-dark text-capitalize">${tipoNormalizado}</small>
+              </div>
+          </div>
+      </div>
+    `;
+
+    contenedor.append(estructuraTarjeta); 
+  });
+}
+
+
 const salt = "AlK3-W4ll3t_T4l3nt0-D1git4L_2026";
 // Redirección
 export function redireccion(enlace, seg) {
